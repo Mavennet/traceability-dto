7b4db764a90af641d3ee27fedea51fd98ea6d7db
vivienfan <vivien.fan.1224@gmail.com>
2021-06-09 12:31:12 -0400

    rm sizeOrAmount from type Product

--- lib/products/dto/product.dto.d.ts
+++ lib/products/dto/product.dto.d.ts
@@ -1,8 +1,7 @@
-import { OrganizationDTO, MeasurementDTO, JSON_TYPE } from '../../general';
+import { OrganizationDTO, JSON_TYPE } from '../../general';
 export declare abstract class ProductDTO {
     type: JSON_TYPE[];
     manufacturer: OrganizationDTO;
     abstract name: string;
     description: string;
-    sizeOrAmount: MeasurementDTO;
 }
