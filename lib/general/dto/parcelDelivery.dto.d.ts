d06f85b29c84ca9bacb3425ed1fc649b6669c926
Zheng <limzhengwei_99@hotmail.com>
2021-06-17 18:39:56 +0800

    Update type to array

--- lib/general/dto/parcelDelivery.dto.d.ts
+++ lib/general/dto/parcelDelivery.dto.d.ts
@@ -1,7 +1,7 @@
 import { JSON_TYPE } from '../constants';
 import { PostalAddressDTO } from './postalAddress.dto';
 export declare abstract class ParcelDeliveryDTO {
-    type: JSON_TYPE;
+    type: JSON_TYPE[];
     originAddress: PostalAddressDTO;
     deliveryAddress: PostalAddressDTO;
     deliveryMethod?: string;
