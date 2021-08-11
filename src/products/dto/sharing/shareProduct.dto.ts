22738de54087403a6f9ca81bf2e7da62d030ab43
vivienfan <vivien.fan.1224@gmail.com>
2021-08-11 02:31:02 -0400

    add swager

--- src/products/dto/sharing/shareProduct.dto.ts
+++ src/products/dto/sharing/shareProduct.dto.ts
@@ -4,12 +4,15 @@ import {
   IsArray,
   ArrayMinSize
 } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class ShareProductDTO {
+  @ApiProperty()
   @IsNotEmpty()
   @IsUUID()
   productId: string
 
+  @ApiProperty()
   @IsArray()
   @ArrayMinSize(0)
   orgs: number[]
