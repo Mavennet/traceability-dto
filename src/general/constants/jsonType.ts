9e7b11d741ae2bf64aa2945a1f7ba5f3cda75f4d
vivienfan <vivien.fan.1224@gmail.com>
2021-09-24 14:57:46 -0400

    update context const val

--- src/general/constants/jsonType.ts
+++ src/general/constants/jsonType.ts
@@ -15,7 +15,7 @@ export enum JSON_TYPE {
   REVOCATION_LIST_2020_STATUS = 'RevocationList2020Status',
   REVOCATION_LIST_2020_CREDENTIAL = 'RevocationList2020Credential',
   REVOCATION_LIST_2020 = 'RevocationList2020',
-  CUSTOM_PRODUCT = 'product-extend',
-  CUSTOM_ORGANIZATION = 'organization-extend',
+  CUSTOM_PRODUCT = 'ExtProduct',
+  CUSTOM_ORGANIZATION = 'ExtOrganization',
   ED25519SIGNATURE2020 = 'Ed25519Signature2020'
 }
