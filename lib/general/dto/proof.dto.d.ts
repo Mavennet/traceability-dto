47284d9a2ff9837b5d7479386a7beac6de267fa6
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2021-11-23 17:44:46 -0500

    fix proof dto values

--- lib/general/dto/proof.dto.d.ts
+++ lib/general/dto/proof.dto.d.ts
@@ -2,7 +2,7 @@ import { JSON_TYPE, PROOF_PURPOSE_TYPE } from '../constants';
 export declare abstract class ProofDTO {
     type: JSON_TYPE;
     created: string;
-    proofPurpose: PROOF_PURPOSE_TYPE;
-    proofValue: string;
-    verificationMethod: string;
+    proofPurpose?: PROOF_PURPOSE_TYPE;
+    proofValue?: string;
+    verificationMethod?: string;
 }
