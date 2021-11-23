a3849ff263d3441394166ddff29c9a600a6f71e5
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2021-11-23 17:37:35 -0500

    update proofdto to be less restrictive

--- src/general/dto/proof.dto.ts
+++ src/general/dto/proof.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsDateString, IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator'
+import { IsDateString, IsEnum, IsNotEmpty, IsString } from 'class-validator'
 import { JSON_TYPE, PROOF_PURPOSE_TYPE } from '../constants'
 
 export abstract class ProofDTO {
@@ -14,18 +14,14 @@ export abstract class ProofDTO {
   created: string
 
   @ApiProperty()
-  @IsNotEmpty()
   @IsEnum(PROOF_PURPOSE_TYPE)
-  proofPurpose: PROOF_PURPOSE_TYPE
+  proofPurpose?: PROOF_PURPOSE_TYPE
 
   @ApiProperty()
-  @IsNotEmpty()
   @IsString()
-  proofValue: string
+  proofValue?: string
 
   @ApiProperty()
-  @IsNotEmpty()
   @IsString()
-  @Matches(/^did:/)
-  verificationMethod: string
+  verificationMethod?: string
 }
