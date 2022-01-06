fc119d5fbdac3498846f96e3d9fa94012bb0a5a3
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2022-01-06 12:23:31 -0500

    Credential validation (#32)
    * updated credential validation
    
    * update credential and presentation validation
    
    * update available pressentation
    
    * update available pressentation
    
    * update issuer DTO
    
    * fix stale lib builds

--- lib/credentials/dto/presentationOptions.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { PROOF_PURPOSE_TYPE } from '../../general';
-export declare class PresentationOptionsDTO {
-    proofPurpose?: PROOF_PURPOSE_TYPE.AUTHENTICATION;
-    verificationMethod?: string;
-    created?: string;
-    challenge?: string;
-    domain?: string;
-    type?: string;
-}
