1058a0bc739838c4c4ff23952a210467cd2e6d91
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2023-01-04 13:41:27 -0500

    update createPresentation

--- lib/presentations/dto/createPresentation.dto.d.ts
+++ lib/presentations/dto/createPresentation.dto.d.ts
@@ -1,4 +1,8 @@
-import { VerifiableCredentialDTO } from '../..';
-export declare class CreatePresentationDTO {
-    verifiableCredential: VerifiableCredentialDTO[];
+import { PresentationDTO as PresentationBase } from '../..';
+declare class Presentation extends PresentationBase {
+    id: string;
 }
+export declare class CreatePresentationDTO {
+    presentation: Presentation;
+}
+export {};
--- src/presentations/dto/createPresentation.dto.ts
+++ src/presentations/dto/createPresentation.dto.ts
@@ -1,8 +1,16 @@
-import { IsNotEmpty, ValidateNested } from 'class-validator'
-import { VerifiableCredentialDTO } from '../..'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsOptional, ValidateNested } from 'class-validator'
+import { PresentationDTO as PresentationBase } from '../..'
+
+class Presentation extends PresentationBase {
+  @ApiProperty()
+  @IsOptional()
+  id: string
+}
 
 export class CreatePresentationDTO {
   @ValidateNested()
   @IsNotEmpty()
-  verifiableCredential: VerifiableCredentialDTO[]
+  @ApiProperty()
+  presentation: Presentation
 }
