b5d12cbc599dd197da98b960d9f43edb77514680
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2023-02-27 12:36:39 -0500

    update packages (#63)

--- src/presentations/dto/presentation.dto.ts
+++ src/presentations/dto/presentation.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { ArrayMinSize, IsArray, IsNotEmpty, IsString, IsUrl, Validate, ValidateNested } from 'class-validator'
-import { VerifiableCredentialDTO } from '../..'
+import type { VerifiableCredentialDTO } from '../..'
 
 export abstract class PresentationDTO {
   @ApiProperty()
