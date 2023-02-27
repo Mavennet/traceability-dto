e705eca30f0047513c189ddbdc89e1943c0dc635
Mahmoud Alkhraishi <mahmoud@mavennet.com>
2023-02-27 16:59:04 -0500

    fix enum + includes

--- src/general/dto/parcelDelivery.dto.ts
+++ src/general/dto/parcelDelivery.dto.ts
@@ -1,15 +1,14 @@
-import { IsOptional, IsEnum, IsString, ValidateNested, IsArray, ArrayNotEmpty, Validate } from 'class-validator'
-import { Type } from 'class-transformer'
-import { JSON_TYPE } from '../constants'
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { Type } from 'class-transformer'
+import { ArrayNotEmpty, IsArray, IsOptional, IsString, Validate, ValidateNested } from 'class-validator'
+import { JSON_TYPE } from '../constants'
 import { PlaceDTO } from './place.dto'
 
 export abstract class ParcelDeliveryDTO {
   @ApiProperty()
   @IsArray()
   @ArrayNotEmpty()
-  @IsEnum(JSON_TYPE, { each: true })
-  @Validate((o) => o.type === [JSON_TYPE.PARCEL_DELIVERY])
+  @Validate((o) => o.type.includes(JSON_TYPE.PARCEL_DELIVERY))
   type: JSON_TYPE[]
 
   @ApiPropertyOptional()
