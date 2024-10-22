15c8e5783c0eae1f0f931a4c2beaec66560673de
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2024-10-22 16:08:02 -0400

    cleanup jwt_types

--- src/general/dto/commodity.dto.ts
+++ src/general/dto/commodity.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
-import { ArrayNotEmpty, IsArray, IsEnum, IsOptional, IsString } from 'class-validator'
+import { ArrayNotEmpty, Equals, IsArray, IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 import { JSON_TYPE } from '../constants'
 
 export class CommodityDTO {
   @ApiProperty()
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsEnum(JSON_TYPE, { each: true })
-  type: JSON_TYPE[]
+  @IsString()
+  @IsNotEmpty()
+  @Equals(JSON_TYPE.COMMODITY)
+  type: JSON_TYPE.COMMODITY
 
   @ApiPropertyOptional()
   @IsOptional()
